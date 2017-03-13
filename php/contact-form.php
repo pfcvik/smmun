6e1831328bd7bc2a5f7942da6aa6e98827d9d11e
Victor Garcia <pfc.vik@gmail.com>
2017-03-13 13:41:08 -0600

    Informacion modificada

--- php/contact-form.php
+++ php/contact-form.php
@@ -7,7 +7,7 @@ header('Content-type: application/json');
 require 'php-mailer/PHPMailerAutoload.php';
 
 // Your email address
-$to = 'youremail@yourserver.com';
+$to = 'registros@smmun.com';
 
 $subject = $_POST['subject'];
 
